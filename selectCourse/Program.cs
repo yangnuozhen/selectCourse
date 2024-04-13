c185538e17ede9701755b62556a1a3a010453a81
yangnuozhen <56541171+yangnuozhen@users.noreply.github.com>
2024-04-14 00:24:03 +0800

    feat: better class selection

--- selectCourse/Program.cs
+++ selectCourse/Program.cs
@@ -8,6 +8,7 @@ internal class Program
     static string AppKey = "02619EF1A99F54F199590E871ED8B9C2";
     static string AccessToken;
     static string GlobalTaskId;
+    static string GlobalClassId;
     private static async Task Main(string[] args)
     {
         Console.WriteLine("=======================================");
@@ -38,18 +39,15 @@ internal class Program
         await selectTargetTask();
         Console.WriteLine("正在获取选课列表...");
         await selectTargetCourse();
-        Console.WriteLine("请输入欲报名的课程对应的ClassID。");
-        Console.WriteLine("ClassID可由上方输出的课程列表复制对应选课的classId取得。");
-        string classIds = Console.ReadLine();
-        Console.WriteLine("请输入尝试间隔(单位: 毫秒, 只输入整数)。建议不要太快(5000以上)，否则可能会被服务器Nginx 429 Too Many Requests");
+        Console.WriteLine("请输入尝试间隔(单位: 毫秒, 只输入整数)。建议不要太快(800以上)，否则可能会被服务器Nginx 429 Too Many Requests");
         int delayTime = int.Parse(Console.ReadLine());
-        Console.WriteLine("===============开始尝试===============");
+        Console.WriteLine("===============开始尝试 请勿关闭命令行窗口===============");
         int times = 1;
         while(true)
         {
             try
             {
-                string back = await PostRequest($"https://gateway.tianwayun.com/apps/course/stu/selectTask/selectTimeCourse?taskId={GlobalTaskId}&classIds={classIds}", AccessToken, AppKey);
+                string back = await PostRequest($"https://gateway.tianwayun.com/apps/course/stu/selectTask/selectTimeCourse?taskId={GlobalTaskId}&classIds={GlobalClassId}", AccessToken, AppKey);
                 
                 Console.WriteLine($"The {times}st try.");
                 Console.WriteLine(back);
@@ -124,7 +122,9 @@ internal class Program
     {
         string url = $"https://gateway.tianwayun.com/apps/course/stu/selectTask/getByTimeTaskId?taskId={GlobalTaskId}";
         string r;
-
+        List<string> className = new List<string>();
+        List<string> teacherName = new List<string>();
+        List<string> classId = new List<string>();
         Console.Write("正在发送请求包...");
         using (HttpClient client = new HttpClient())
         {
@@ -150,14 +150,21 @@ internal class Program
         int UserListCount = json["data"]["courseDTOS"][0]["classDTOS"].Count();
         for (int i = 0; i < UserListCount; i++)
         {
-            
-            Console.WriteLine($"[{i+1}]\n | 课程名称:{json["data"]["courseDTOS"][0]["classDTOS"][i]["className"]}" );
-            Console.WriteLine($" | 授课教师:{json["data"]["courseDTOS"][0]["classDTOS"][i]["teacherName"]}");
-            Console.WriteLine($" | ClassID:{json["data"]["courseDTOS"][0]["classDTOS"][i]["classId"]}");
+            className.Add(json["data"]["courseDTOS"][0]["classDTOS"][i]["className"].ToString());
+            teacherName.Add(json["data"]["courseDTOS"][0]["classDTOS"][i]["teacherName"].ToString());
+            classId.Add(json["data"]["courseDTOS"][0]["classDTOS"][i]["classId"].ToString());
+            Console.WriteLine($"[{i+1}]\n | 课程名称:{className[i]}" );
+            Console.WriteLine($" | 授课教师:{teacherName[i]}");
+            Console.WriteLine($" | ClassID:{classId[i]}");
             Console.WriteLine();
             
         }
         Console.WriteLine("==============================================");
+        Console.WriteLine("请选择课程，并输入课程前的序号: ");
+        int select = int.Parse(Console.ReadLine());
+        GlobalClassId = classId[select - 1];
+
+        Console.WriteLine($"成功, 当前已选择课程: {className[select - 1]}。");
     }
     private static async Task selectTargetTask()
     {
